382006d2970130e4dd55d41f25dd86fe012da17c
Alexander Menk <alex.menk@gmail.com>
2013-09-08 16:40:04 +0200

    New: exif-date-rename.py

---
+++ exif-date-rename.py
@@ -0,0 +1,22 @@
+import os, string, sys, Image
+
+#renames photos so they contain the date of recording in the filename
+
+def get_exif_data(file):
+        image = Image.open(file)
+        exif_data = image._getexif()
+        exif = exif_data[36868]
+        return '%s-%s-%s' % (exif[:4], exif[5:7], exif[8:10]) 
+
+
+if len(sys.argv) == 1:  # wenn keine Dateinamen angegeben sind,
+    filenames = os.listdir(os.curdir)  # nimm das aktuelle Verzeichnis
+
+else:  # ansonsten die angegebenen Dateien
+    filenames = sys.argv[1:]  # aus der Kommandozeile
+
+for filename in filenames:
+    newfilename = '%s_%s' % (get_exif_data(filename) , filename)
+    os.rename(filename, newfilename)
+
+
