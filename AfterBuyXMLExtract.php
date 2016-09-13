9e274121395b3e8d612921ab522d688e135b2376
Alexander Menk <alex.menk@gmail.com>
2016-09-13 16:52:12 +0200

    Afterbuy XML Extract Script

---
+++ AfterBuyXMLExtract.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Extracts an afterbuy XML file
+ */
+$xml = simplexml_load_file('AfterBuyDump.xml');
+
+foreach($xml->ShopDesign->children() as $name=>$element) {
+    $name = 'dump/' . $name;
+    if (strpos($name, 'XSL') !== false) {
+        $name .= '.xsl';
+    }
+    file_put_contents($name, base64_decode((string)$element));
+}
