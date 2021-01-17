f095a98ec32f8bfd611fe15bb005dafb6b9ec8d3
Alexander Menk <alex.menk@gmail.com>
2021-01-17 17:18:19 +0100

    Import PIL, add exception handling

--- exif-date-rename.py
+++ exif-date-rename.py
@@ -1,12 +1,16 @@
-import os, string, sys, Image
+import os, string, sys
+from PIL import Image
 
 #renames photos so they contain the date of recording in the filename
 
 def get_exif_data(file):
         image = Image.open(file)
         exif_data = image._getexif()
-        exif = exif_data[36868]
-        return '%s-%s-%s' % (exif[:4], exif[5:7], exif[8:10]) 
+        try:
+            exif = exif_data[36868]
+            return '%s-%s-%s' % (exif[:4], exif[5:7], exif[8:10]) 
+        except (KeyError, TypeError):
+            return '0000-00-00';
 
 
 if len(sys.argv) == 1:  # wenn keine Dateinamen angegeben sind,
